033bcc2615ecc7e24c9198080bdeecd83f485fec
Olivier, Clement <clement.olivier@bearingpoint.com>
2017-06-14 09:40:08 +0200

    Plugin + ressources first commit

---
+++ DeltaAssemblyvsCrm/DeltaAssemblyvsCrm.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using System.Reflection;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Carfup.XTBPlugins.DeltaAssemblyvsCrm
+{
+	public partial class DeltaAssemblyvsCrm : PluginControlBase
+	{
+		public string assemblyName = "";
+		private string[] listOfCRMAssemblies = null;
+		private string[] listOfPluginsInCRM = null;
+		private string[] listOfPluginsInAssembly = null;
+		private string[] inCRMButAssembly = null;
+		private string[] inAssemblyButCRM = null;
+
+		public DeltaAssemblyvsCrm()
+		{
+			InitializeComponent();
+
+			initDisplayItems();
+			
+		}
+
+		public void initDisplayItems()
+		{
+			//tabControl1.Width = 
+			//tabControl1.Height = Unit.Percentage(100);
+		}
+
+		private void toolStripButtonLoadPluginStepsClick(object sender, EventArgs evt)
+		{
+			if (Service == null)
+				MessageBox.Show("Please get connected to an environment first.");
+			else
+			{
+				// we clear the content of the list first
+				comboBoxAssemblyList.Items.Clear();
+
+				WorkAsync(new WorkAsyncInfo
+				{
+					Message = "Loading CRM Assemblies...",
+					Work = (bw, e) =>
+					{
+						listOfCRMAssemblies = Service.RetrieveMultiple(new QueryExpression("pluginassembly")
+						{
+							ColumnSet = new ColumnSet(true),
+							Criteria = new FilterExpression
+							{
+								Conditions =
+									{
+									new ConditionExpression("name", ConditionOperator.DoesNotBeginWith, "Microsoft."),
+									new ConditionExpression("name", ConditionOperator.DoesNotBeginWith, "ActivityFeeds.")
+									}
+							}
+						}).Entities.Select(p => p.Attributes["name"].ToString()).ToArray();
+					},
+					PostWorkCallBack = e =>
+					{
+						if (e.Error != null)
+						{
+							MessageBox.Show(this, e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+							return;
+						}
+
+						if (listOfCRMAssemblies != null)
+						{
+							comboBoxAssemblyList.Items.AddRange(listOfCRMAssemblies);
+							comboBoxAssemblyList.Focus();
+						}
+					},
+					ProgressChanged = e => { SetWorkingMessage(e.UserState.ToString()); }
+				});
+			}
+		}
+
+		private void buttonLoadAssemblyClick(object sender, EventArgs e)
+		{
+			// we clear the content of the list first
+			listBoxPluginTypesAssembly.Items.Clear();
+
+			// opening the dialog box to select the dll
+			openFileDialogLoadAssembly.ShowDialog();
+			string filepath = openFileDialogLoadAssembly.FileName;
+			labelLoadAssembly.Visible = true;
+			labelLoadAssembly.Text = $"Your assembly : {filepath.Split('\\').Last()}";
+
+			AppDomain domain = AppDomain.CreateDomain(labelLoadAssembly.Text);
+
+			System.IO.StreamReader reader = new System.IO.StreamReader(filepath, System.Text.Encoding.GetEncoding(1252), false);
+
+			byte[] b = new byte[reader.BaseStream.Length];
+			reader.BaseStream.Read(b, 0, System.Convert.ToInt32(reader.BaseStream.Length));
+
+			reader.Close();
+
+
+			WorkAsync(new WorkAsyncInfo
+			{
+				Message = "loading the assembly Plugins...",
+				Work = (bw, evt) =>
+				{
+					listOfPluginsInAssembly = Assembly.Load(b).GetTypes()
+					.Where(t => !(t.FullName.Contains("<>c")) && (t.GetInterfaces().Contains(typeof(IPlugin)) || t.GetInterfaces().Contains(typeof(IPluginExecutionContext))))
+					.Select(t => t.FullName)
+					.ToArray();
+
+					
+					
+				},
+				PostWorkCallBack = evt =>
+				{
+					if (evt.Error != null)
+					{
+						MessageBox.Show(this, evt.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						return;
+					}
+
+					if(listOfPluginsInAssembly != null)
+						listBoxPluginTypesAssembly.Items.AddRange(listOfPluginsInAssembly);
+
+					if (listBoxPluginTypes.Items.Count > 0 && listBoxPluginTypesAssembly.Items.Count > 0)
+						toolStripButtonCompare.Visible = true;
+				},
+				ProgressChanged = evt => { SetWorkingMessage(evt.UserState.ToString()); }
+			});
+
+			AppDomain.Unload(domain);
+		}
+
+		private void toolStripButtonCompare_Click(object sender, EventArgs e)
+		{
+			// we clear the content of the list first
+			listBoxInAssemblyButCRM.Items.Clear();
+			listBoxInCRMButAssembly.Items.Clear();
+
+			WorkAsync(new WorkAsyncInfo
+			{
+				Message = "Comparing Plugins...",
+				Work = (bw, evt) =>
+				{
+					inCRMButAssembly = listOfPluginsInCRM.Except(listOfPluginsInAssembly).ToArray();
+					inAssemblyButCRM = listOfPluginsInAssembly.Except(listOfPluginsInCRM).ToArray();
+
+					
+				},
+				PostWorkCallBack = evt =>
+				{
+					if (evt.Error != null)
+					{
+						MessageBox.Show(this, evt.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						return;
+					}
+
+					if (inCRMButAssembly != null && inAssemblyButCRM != null)
+					{
+						if (inCRMButAssembly.Count() == 0)
+						{
+							listBoxInCRMButAssembly.Visible = false;
+							labelCrmButAssemblyMatch.Visible = true;
+						}
+						else
+						{
+							listBoxInCRMButAssembly.Visible = true;
+							listBoxInCRMButAssembly.Items.AddRange(inCRMButAssembly);
+							labelCrmButAssemblyMatch.Visible = false;
+
+						}
+
+						if (inAssemblyButCRM.Count() == 0)
+						{
+							listBoxInAssemblyButCRM.Visible = false;
+							labelAssemblyButCRMMatch.Visible = true;
+						}
+						else
+						{
+							listBoxInAssemblyButCRM.Items.AddRange(inAssemblyButCRM);
+							listBoxInAssemblyButCRM.Visible = true;
+							labelAssemblyButCRMMatch.Visible = false;
+						}
+						tabControl1.SelectedTab = tabPageResult;
+					}
+				},
+				ProgressChanged = evt => { SetWorkingMessage(evt.UserState.ToString()); }
+			});
+		}
+
+		private void comboBoxAssemblyList_Changed(object sender, EventArgs evt)
+		{
+			// we clear the content of the list first
+			listBoxPluginTypes.Items.Clear();
+
+			var assemblyname = comboBoxAssemblyList.SelectedItem.ToString();
+			
+			WorkAsync(new WorkAsyncInfo
+			{
+				Message = "Loading Plugins...",
+				Work = (bw, e) =>
+				{
+				listOfPluginsInCRM = Service.RetrieveMultiple(new QueryExpression("plugintype")
+				{
+					ColumnSet = new ColumnSet(true),
+					Criteria = new FilterExpression
+					{
+						Conditions =
+								{
+									new ConditionExpression("assemblyname", ConditionOperator.Equal, assemblyname)
+								}
+						}
+					}).Entities.Select(p => p.Attributes["typename"].ToString()).ToArray();
+				},
+				PostWorkCallBack = e =>
+				{
+					if (e.Error != null)
+					{
+						MessageBox.Show(this, e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						return;
+					}
+
+					if (listOfPluginsInCRM != null)
+						listBoxPluginTypes.Items.AddRange(listOfPluginsInCRM);
+
+					if (listBoxPluginTypes.Items.Count > 0 && listBoxPluginTypesAssembly.Items.Count > 0)
+						toolStripButtonCompare.Visible = true;
+				},
+				ProgressChanged = e => { SetWorkingMessage(e.UserState.ToString()); }
+			});
+		}
+
+		private void toolStripButtonCloseTool_Click(object sender, EventArgs e)
+		{
+			CloseTool();
+		}
+	}
+}
---
+++ DeltaAssemblyvsCrm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Delta Assembly vs CRM")]
+[assembly: AssemblyDescription("Find out which Plugin differences you have between your CRM Assembly and your Local one.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Clement O.")]
+[assembly: AssemblyProduct("Carfup.Plugins.DeltaAssemblyvscrm")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b0a33b0-c87e-4b9b-89bb-0227991c93a4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
